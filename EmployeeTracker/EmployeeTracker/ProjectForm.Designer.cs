76e51623a2c5c06cfb1808c489f995b84f307571
Chris Fuess <cfuess@LIVE.COM>
2017-11-04 10:31:54 -0700

    Prep for class

---
+++ EmployeeTracker/EmployeeTracker/ProjectForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace EmployeeTracker
+{
+    partial class ProjectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dtStartDate = new System.Windows.Forms.DateTimePicker();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtEndDate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstTechnologies = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(381, 195);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(46, 17);
+            this.lblResult.TabIndex = 15;
+            this.lblResult.Text = "label4";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(450, 195);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(119, 34);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dtStartDate
+            // 
+            this.dtStartDate.Location = new System.Drawing.Point(113, 141);
+            this.dtStartDate.Margin = new System.Windows.Forms.Padding(2);
+            this.dtStartDate.Name = "dtStartDate";
+            this.dtStartDate.Size = new System.Drawing.Size(241, 22);
+            this.dtStartDate.TabIndex = 13;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(113, 75);
+            this.txtDescription.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(241, 55);
+            this.txtDescription.TabIndex = 12;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(113, 42);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(136, 22);
+            this.txtName.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 141);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Start Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Description";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Project Name";
+            // 
+            // dtEndDate
+            // 
+            this.dtEndDate.Location = new System.Drawing.Point(113, 174);
+            this.dtEndDate.Margin = new System.Windows.Forms.Padding(2);
+            this.dtEndDate.Name = "dtEndDate";
+            this.dtEndDate.Size = new System.Drawing.Size(241, 22);
+            this.dtEndDate.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 174);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "End Date";
+            // 
+            // lstTechnologies
+            // 
+            this.lstTechnologies.FormattingEnabled = true;
+            this.lstTechnologies.ItemHeight = 16;
+            this.lstTechnologies.Location = new System.Drawing.Point(388, 13);
+            this.lstTechnologies.Margin = new System.Windows.Forms.Padding(2);
+            this.lstTechnologies.Name = "lstTechnologies";
+            this.lstTechnologies.Size = new System.Drawing.Size(181, 164);
+            this.lstTechnologies.TabIndex = 18;
+            this.lstTechnologies.Click += new System.EventHandler(this.lstTechnologies_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 17);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(113, 11);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(59, 22);
+            this.txtId.TabIndex = 23;
+            // 
+            // ProjectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 240);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lstTechnologies);
+            this.Controls.Add(this.dtEndDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dtStartDate);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ProjectForm";
+            this.Text = "Project";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DateTimePicker dtStartDate;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtEndDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstTechnologies;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtId;
+    }
+}
---
+++ EmployeeTracker/EmployeeTracker/ProjectForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EmployeeTracker.Models;
+
+namespace EmployeeTracker
+{
+    public partial class ProjectForm : Form
+    {
+        private readonly List<Project> _projects;
+        private Project _project = new Project();
+
+
+        public ProjectForm(List<Project> projects)
+        {
+            InitializeComponent();
+            _projects = projects;
+            txtId.Text = GetNextId().ToString();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            _project.Id = int.Parse(txtId.Text);
+            _project.Name = txtName.Text;
+            _project.Description = txtDescription.Text;
+            _project.StartDate = DateTime.Parse(dtStartDate.Text);
+            _project.EndDate = DateTime.Parse(dtEndDate.Text);
+            _projects.Add(_project);
+        }
+
+        private void loadTechnologies()
+        {
+            // clear the list
+            lstTechnologies.Items.Clear();
+
+            // put all the technologies in the list
+            foreach (var tech in _project.Technologies)
+            {
+                lstTechnologies.Items.Add(tech);
+            }
+        }
+
+        private void results_ShowResults(string message)
+        {
+            lblResult.Text = message;
+        }
+
+        private void lstTechnologies_Click(object sender, EventArgs e)
+        {
+            string tech = Prompt.ShowDialog("Technology", "Add");
+            _project.Technologies.Add(tech);
+            loadTechnologies();
+        }
+
+        public int GetNextId()
+        {
+            if (!_projects.Any())
+            {
+                return 0;
+            }
+            else
+            {
+                return _projects.Max(p => p.Id) + 1;
+            }
+        }
+
+
+    }
+}
